ddec6270e0d2bda735625925ac2e281509289d7c
Shasidhar Karumuri <135424397+shasidhar7@users.noreply.github.com>
2025-02-23 22:25:43 +0530

    Delete FetchAPIs directory

--- FetchAPIs/fetchdata.js
+++
@@ -1,7 +0,0 @@
-const resp = fetch('https://jsonplaceholder.typicode.com/users')
-
-resp
-.then(resp=>console.log(resp.status))
-.catch(err=>console.log(err))
-.finally(()=>console.log('Promise Executed'))
-
